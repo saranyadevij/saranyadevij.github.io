83b14016217220f515d8d61f4ce495d90cbcd649
saran <48084964+saranyadevij@users.noreply.github.com>
2023-03-25 12:49:23 +0700

    fixed some responsive issues

--- js/main.js
+++ js/main.js
@@ -1,36 +1,40 @@
 $(document).ready(function () {
-  // change active menu in navbar
-  $("a").click(function () {
-    $("a").removeClass("active");
-    $(this).addClass("active");
-  });
+ // change active menu in navbar
+ $("a").click(function () {
+  $("a").removeClass("active");
+  $(this).addClass("active");
+ });
 
-  var $grid1 = $(".grid1").isotope({});
+ var $grid1 = $(".grid1").isotope({});
 
-  $(".filter-button-group").on("click", "button", function () {
-    var filterValue = $(this).attr("data-filter");
-    $grid1.isotope({ filter: filterValue });
-  });
+ $(".filter-button-group").on("click", "button", function () {
+  var filterValue = $(this).attr("data-filter");
+  $grid1.isotope({ filter: filterValue });
+ });
 
-  $(".test-popup-link").magnificPopup({
-    type: "image",
-    gallery: { enabled: true },
-  });
+ $(".test-popup-link").magnificPopup({
+  type: "image",
+  gallery: { enabled: true },
+ });
 
-  // sticky Nav Menu
-  let nav_offset_top = $(".header_area").height() + 30;
+ // sticky Nav Menu
+ let nav_offset_top = $(".header_area").height() + 30;
 
-  function navbarFixed() {
-    if ($(".header_area").length) {
-      $(window).scroll(function () {
-        let scroll = $(window).scrollTop();
-        if (scroll >= nav_offset_top) {
-          $(".header_area .main-menu").addClass("navbar_fixed");
-        } else {
-          $(".header_area .main-menu").removeClass("navbar_fixed");
-        }
-      });
+ function navbarFixed() {
+  if ($(".header_area").length) {
+   $(window).scroll(function () {
+    let scroll = $(window).scrollTop();
+    if (scroll >= nav_offset_top) {
+     $(".header_area .main-menu").addClass("navbar_fixed");
+    } else {
+     $(".header_area .main-menu").removeClass("navbar_fixed");
     }
+   });
   }
-  navbarFixed();
+ }
+ navbarFixed();
+});
+
+$(document).on("click", function () {
+ $(".collapse").collapse("hide");
 });
