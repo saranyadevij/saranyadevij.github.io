e2ae90fd3f88a5a00050da01275fe02804206d2f
saran <48084964+saranyadevij@users.noreply.github.com>
2023-04-18 14:27:15 +0700

    portfolio webpage created

--- js/main.js
+++ js/main.js
@@ -38,3 +38,26 @@ $(document).ready(function () {
 $(document).on("click", function () {
  $(".collapse").collapse("hide");
 });
+
+function sendMail() {
+ var params = {
+  name: document.getElementById("name").value,
+  email: document.getElementById("email").value,
+  subject: document.getElementById("subject").value,
+  message: document.getElementById("message").value,
+ };
+
+ const serviceID = "service_063i9qd";
+ const templateID = "template_7ozfa8h";
+ emailjs
+  .send(serviceID, templateID, params)
+  .then((res) => {
+   document.getElementById("name").value = "";
+   document.getElementById("email").value = "";
+   document.getElementById("subject").value = "";
+   document.getElementById("message").value = "";
+   console.log(res);
+   alert("Your message sent successfully");
+  })
+  .catch((err) => console.log(err));
+}
